df7c17c575115e2099bdb89199e81c414903b43c
Abhishek Sah <abhi.sah.97@gmail.com>
2024-09-30 20:37:46 +0530

    fetch mails IDs matching filter pattern

--- main.py
+++ main.py
@@ -8,11 +8,34 @@ from googleapiclient.errors import HttpError
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
+INBOX_SEARCH_FILTER = "from:alerts@hdfcbank.net after:2024/8/30 before:2024/10/31 is:read"
+
+def fetch_all_mail_ids(service):
+  results = service.users().messages().list(userId="me", q=INBOX_SEARCH_FILTER).execute()
+  mails = results.get("messages", [])
+  next_page_token = results["nextPageToken"]
+  res = []
+  mail_ids = []
+  res.append(results)
+
+  print(f"len of mails {len(mails)}")
+  while next_page_token != "":
+    results = service.users().messages().list(userId="me", q=INBOX_SEARCH_FILTER, pageToken=next_page_token).execute()
+    res.append(results)
+    if "nextPageToken" in results:
+      next_page_token = results["nextPageToken"]
+    else:
+      next_page_token = ""
+
+  for item in res:
+    for mails in item["messages"]:
+      mail_ids.append(mails["id"])
+  return mail_ids
 
 
 def main():
-  """Shows basic usage of the Gmail API.
-  Lists the user's Gmail labels.
+  """Fetches mails matching the query pattern,
+  parses them to prepare the spend reports
   """
   creds = None
   # The file token.json stores the user's access and refresh tokens, and is
@@ -34,18 +57,9 @@ def main():
       token.write(creds.to_json())
 
   try:
-    # Call the Gmail API
     service = build("gmail", "v1", credentials=creds)
-    results = service.users().labels().list(userId="me").execute()
-    labels = results.get("labels", [])
-
-    if not labels:
-      print("No labels found.")
-      return
-    print("Labels:")
-    for label in labels:
-      print(label["name"])
-
+    mail_ids = fetch_all_mail_ids(service)
+    print(mail_ids)
   except HttpError as error:
     # TODO(developer) - Handle errors from gmail API.
     print(f"An error occurred: {error}")
